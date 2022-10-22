0e7d3003622ddc4208e90aec9b6077da69c7f067
thiagola92 <thiagola92@gmail.com>
2022-10-22 02:24:31 -0300

    Add 2 templates and update olds templates

--- issue_template.tf
+++ issue_template.tf
@@ -10,13 +10,37 @@ resource "github_repository_file" "bug_report" {
   overwrite_on_create = true
 }
 
-resource "github_repository_file" "feature_request" {
+resource "github_repository_file" "enhancement_proposal" {
   for_each            = toset(data.github_repositories.la_catalog.names)
   repository          = each.key
   branch              = "main"
-  file                = ".github/ISSUE_TEMPLATE/feature_request.md"
-  content             = file("./.github/ISSUE_TEMPLATE/feature_request.md")
-  commit_message      = "Update feature_request.md"
+  file                = ".github/ISSUE_TEMPLATE/enhancement_proposal.md"
+  content             = file("./.github/ISSUE_TEMPLATE/enhancement_proposal.md")
+  commit_message      = "Update enhancement_proposal.md"
+  commit_author       = "actions"
+  commit_email        = "actions@github.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "feature_proposal" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/ISSUE_TEMPLATE/feature_proposal.md"
+  content             = file("./.github/ISSUE_TEMPLATE/feature_proposal.md")
+  commit_message      = "Update feature_proposal.md"
+  commit_author       = "actions"
+  commit_email        = "actions@github.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "problem_report" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/ISSUE_TEMPLATE/problem_report.md"
+  content             = file("./.github/ISSUE_TEMPLATE/problem_report.md")
+  commit_message      = "Update problem_report.md"
   commit_author       = "actions"
   commit_email        = "actions@github.com"
   overwrite_on_create = true

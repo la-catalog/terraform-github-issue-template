00fb2e21a1dafa9c9cff63223339b903f7f028a0
thiagola92 <thiagola92@gmail.com>
2022-10-22 15:21:23 -0300

    Add missing configuration in terraform file

--- issue_template.tf
+++ issue_template.tf
@@ -45,3 +45,39 @@ resource "github_repository_file" "problem_report" {
   commit_email        = "github-actions[bot]@users.noreply.github.com"
   overwrite_on_create = true
 }
+
+resource "github_repository_file" "refactor_proposal" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/ISSUE_TEMPLATE/refactor_proposal.md"
+  content             = file("./.github/ISSUE_TEMPLATE/refactor_proposal.md")
+  commit_message      = "Update refactor_proposal.md"
+  commit_author       = "github-actions"
+  commit_email        = "github-actions[bot]@users.noreply.github.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "clean_proposal" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/ISSUE_TEMPLATE/clean_proposal.md"
+  content             = file("./.github/ISSUE_TEMPLATE/clean_proposal.md")
+  commit_message      = "Update clean_proposal.md"
+  commit_author       = "github-actions"
+  commit_email        = "github-actions[bot]@users.noreply.github.com"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "question" {
+  for_each            = toset(data.github_repositories.la_catalog.names)
+  repository          = each.key
+  branch              = "main"
+  file                = ".github/ISSUE_TEMPLATE/question.md"
+  content             = file("./.github/ISSUE_TEMPLATE/question.md")
+  commit_message      = "Update question.md"
+  commit_author       = "github-actions"
+  commit_email        = "github-actions[bot]@users.noreply.github.com"
+  overwrite_on_create = true
+}
